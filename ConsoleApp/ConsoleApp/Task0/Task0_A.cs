b9dfe60388efc151257bf53774cf1d75fa683b33
svanski <dachge@gmail.com>
2014-01-28 23:18:03 -0500

    Task0_A has been added

---
+++ ConsoleApp/ConsoleApp/Task0/Task0_A.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Task0
+{
+    /* this is a class */
+    public class Task0_A
+    {
+
+        /*
+         * You need to memorize and know by heart what is a method and struction of the method.
+         *  Method aka Function. they can be used interchangably I think C# calls them methods while some
+         *  other programming language may call them functions. But they mean same and we can use them interchangably
+         * 
+         * The sturcture is 
+         *      AccessModifier ReturnType NameOfTheMethod(Parametr(s)) {     return ReturnType;}
+         * Ex:
+         * 
+         *      public string HelloWorldFunction( string StringParameter, int intParameter) 
+         *      {   
+         *          return "";
+         *      }
+         * 
+         * public               = AccessModifier  http://msdn.microsoft.com/en-us/library/wxh6fsc7.aspx
+         * string               = return type     http://msdn.microsoft.com/en-us/library/ms228360(v=vs.90).aspx
+         * HelloWorldFunction   = method Name     Method, Function same thing...
+         * stringParameter      = parameter1 of type string
+         * intParameter         = parameter2 of type int
+         * return ""            = returns empty string
+         * 
+         * 
+         * There are limited number of AccessModifiers you can read about them from the link above. For now we can 
+         *           just focuse on public and do not worry too much about others
+         *           
+         * There are numberous ReturnTypes. ReturnTypes could be any Type. The link has list of all premetive dataTypes that C# 
+         *           comes with (If I do not explain something; for example you might ask what does premitive type means for the
+         *                        language feel free to goodle it. Actually that would be one of the excersises)
+         *                        
+         * The name of the function could be anything. By the C# convention All the names start with the Upper case and every word
+         *          in the name is also capitilized ex: JustLikeThis
+         *          
+         * Just like ReturnType parameters could be any type. You can notice that in example function return type and 1st parameter
+         *          have same type.
+         *          
+         * Whatever function return type is must match what the function returns. For example if return type is int you need to 
+         *          return int. if it is string then function should return string.
+         *          
+         * Read this link http://msdn.microsoft.com/en-us/library/ms173114.aspx it talks about functions and once you are done
+         *           
+         * 
+         */
+
+
+        /*
+         * Write a function to who access is not restercted, returns intiger, HasSomeNameWhateverYouWant, and takes in 3 paramters
+         *          of type int, int and string
+         * 
+         * AccessModifier   ReturnType Name( parameter1sType Parameter1sName, parameter2sType Parameter2sName, parameter3sType Parameter3sName)
+         * {
+         * 
+         * 
+         *      return SomethigTHatIsAReturnType
+         * }
+         * 
+         */
+
+
+
+
+
+
+
+
+        public string Foo()
+        {
+
+            /*
+             * define 5 string variables 2 of whole must be initialized
+             */
+
+            /*
+             * define 2 boolean variables 
+             */
+
+            /*
+             * QUESTON1: What is a default value of boolean variable
+             * ANSWER
+             * 
+             */
+
+            /*
+             * QUESTION2: What will happen if you were to use uninitialized variable
+             * ANSWER:
+             * 
+             */
+
+
+
+            /*
+             * define 5 intiger variables 3 of them initialized
+             */
+
+            /*
+             * QUESTION3: What is the premitive type and do not give me copy/paste answer if you do not understand 
+             *              You need to understand it not memorize it
+             * ANSWER:
+             */
+
+
+
+
+            return "";
+        }
+
+
+    }
+}
